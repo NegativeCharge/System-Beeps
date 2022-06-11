a8e3a0703b628fb2396d9538d703c817fbaace55
Negative Charge <negativecharge6502@gmail.com>
2022-06-11 15:28:48 +0100

    Restructure project

--- system_beeps_bbc/make.bat
+++ system_beeps_bbc/make.bat
@@ -1,6 +1,6 @@
-cd ".\system_beeps\screens"
+cd ".\system_beeps_bbc\screens"
 IF NOT EXIST cover.exo ..\Tools\exomizer\v3.1.0\exomizer.exe level -M256 -P+16-32 -c cover.bin@0x0000 -o cover.exo
 IF NOT EXIST menu.exo ..\Tools\exomizer\v3.1.0\exomizer.exe level -M256 -P+16-32 -c menu.bin@0x0000 -o menu.exo
 
 cd ..\..
-BeebAsm.exe -i .\system_beeps\loader.s.6502 -do .\system_beeps_bbc.ssd -opt 3
+BeebAsm.exe -i .\system_beeps_bbc\loader.s.6502 -do .\system_beeps_bbc.ssd -opt 3
