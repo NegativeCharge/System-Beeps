faf787ec37fb8cd97c9ad632cef4ded7d801ba43
Negative Charge <negativecharge6502@gmail.com>
2022-06-11 15:24:00 +0100

    v1.0

--- system_beeps/make.bat
+++ system_beeps/make.bat
@@ -3,4 +3,4 @@ IF NOT EXIST cover.exo ..\Tools\exomizer\v3.1.0\exomizer.exe level -M256 -P+16-3
 IF NOT EXIST menu.exo ..\Tools\exomizer\v3.1.0\exomizer.exe level -M256 -P+16-32 -c menu.bin@0x0000 -o menu.exo
 
 cd ..\..
-BeebAsm.exe -i .\system_beeps\loader.s.6502 -do .\system_beeps.ssd -opt 3
+BeebAsm.exe -i .\system_beeps\loader.s.6502 -do .\system_beeps_bbc.ssd -opt 3
